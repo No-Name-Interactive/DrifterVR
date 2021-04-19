6fb7edfa051ac4448465208d6c7b37688fcf26a4
Vari <72755263+Variapolis@users.noreply.github.com>
2021-04-19 16:36:44 +0100

    Collision and AI Follow Nav

---
+++ DrifterCW2/Intermediate/Build/Win64/DrifterCW2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Vari/Desktop/Work/GitHub Temp/NNI-Project-2/DrifterCW2/Intermediate/Build/Win64/DrifterCW2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ DrifterCW2/Intermediate/Build/Win64/UE4Editor/Inc/DrifterCW2/TestPawn.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DRIFTERCW2_TestPawn_generated_h
+#error "TestPawn.generated.h already included, missing '#pragma once' in TestPawn.h"
+#endif
+#define DRIFTERCW2_TestPawn_generated_h
+
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_SPARSE_DATA
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_RPC_WRAPPERS
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATestPawn(); \
+	friend struct Z_Construct_UClass_ATestPawn_Statics; \
+public: \
+	DECLARE_CLASS(ATestPawn, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DrifterCW2"), NO_API) \
+	DECLARE_SERIALIZER(ATestPawn)
+
+
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesATestPawn(); \
+	friend struct Z_Construct_UClass_ATestPawn_Statics; \
+public: \
+	DECLARE_CLASS(ATestPawn, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DrifterCW2"), NO_API) \
+	DECLARE_SERIALIZER(ATestPawn)
+
+
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATestPawn(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATestPawn) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATestPawn); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestPawn); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATestPawn(ATestPawn&&); \
+	NO_API ATestPawn(const ATestPawn&); \
+public:
+
+
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATestPawn(ATestPawn&&); \
+	NO_API ATestPawn(const ATestPawn&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATestPawn); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestPawn); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATestPawn)
+
+
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_PRIVATE_PROPERTY_OFFSET
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_9_PROLOG
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_PRIVATE_PROPERTY_OFFSET \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_SPARSE_DATA \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_RPC_WRAPPERS \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_INCLASS \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define DrifterCW2_Source_DrifterCW2_TestPawn_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_PRIVATE_PROPERTY_OFFSET \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_SPARSE_DATA \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_INCLASS_NO_PURE_DECLS \
+	DrifterCW2_Source_DrifterCW2_TestPawn_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DRIFTERCW2_API UClass* StaticClass<class ATestPawn>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID DrifterCW2_Source_DrifterCW2_TestPawn_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
