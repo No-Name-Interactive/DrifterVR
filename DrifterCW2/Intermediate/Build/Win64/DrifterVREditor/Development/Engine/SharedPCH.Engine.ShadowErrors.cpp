1d1b8aa081d5d0a9224e7a183b01c2fbae68a2b2
Vari <72755263+Variapolis@users.noreply.github.com>
2021-05-06 23:27:31 +0100

    Loading Magazines + Fire Rate Working
    Added FireRate selectable [in-editor only] as well as loading magazines with working but non-animated gun models.

---
+++ DrifterCW2/Intermediate/Build/Win64/DrifterVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Vari/Desktop/Work/GitHub Temp/NNI-Project-2/DrifterCW2/Intermediate/Build/Win64/DrifterVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ DrifterCW2/Intermediate/Build/Win64/DrifterVREditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Vari/Desktop/Work/GitHub Temp/NNI-Project-2/DrifterCW2/Intermediate/Build/Win64/DrifterVREditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
